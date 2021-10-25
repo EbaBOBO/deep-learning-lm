a071fd856997be0ecbfa67e13d26d78c4ab11b07
EbaBOBO <chi_zhang2@brown.edu>
2021-10-24 21:55:42 -0400

    trigram

--- hw3/code/trigram.py
+++ hw3/code/trigram.py
@@ -58,7 +58,7 @@ class Model(tf.keras.Model):
         """
         #TODO: Fill in
 
-        return tf.reduce_mean(tf.keras.metrics.sparse_categorical_crossentropy(labels, probs, from_logits=True, axis=-1))
+        return tf.reduce_mean(tf.keras.metrics.sparse_categorical_crossentropy(labels, probs))
         # return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, probs))
         pass
 
